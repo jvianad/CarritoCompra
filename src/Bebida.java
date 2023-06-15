be864acc1b5a732971e46a7027941e74a319f3db
jvianad <jeanvianademares@gmail.com>
2023-06-14 20:01:55 -0500

    Cambios en rama Jean proximas a master

--- src/Bebida.java
+++ src/Bebida.java
@@ -2,4 +2,6 @@ public class Bebida {
     private String marca;
     private double capacidad;
     private double name;
+    //se agrega atributo color bebida para prueba git
+    private String colorBebida;
 }
--- src/Comida.java
+++ src/Comida.java
@@ -2,4 +2,6 @@ public class Comida {
     private String nombre;
     private String apellido;
     private int edad;
+    //atributo nombre2 para rama jean
+    private String nombre2;
 }
